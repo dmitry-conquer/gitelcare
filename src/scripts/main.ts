cbad561daa28c14a9ffbd7cf8dac0e25e56f1019
Dmitry Conquer <dmitry.conquer@gmail.com>
2025-07-17 15:06:37 +0300

    Services

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -4,6 +4,39 @@ import { Header } from "./components/Header";
 
 const initUIComponents = (): void => {
   new Header();
+
+  //@ts-expect-error Swiper is declared globally
+  new Swiper("#c-services-slider", {
+    slidesPerView: 4,
+    spaceBetween: 32,
+    speed: 1100,
+    pagination: {
+      el: ".c-services__pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".c-services__navigation-button--next",
+      prevEl: ".c-services__navigation-button--prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1.2,
+        spaceBetween: 10,
+      },
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 16,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 32,
+      },
+    },
+  });
 };
 
 document.addEventListener("DOMContentLoaded", (): void => {
